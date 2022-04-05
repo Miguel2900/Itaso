134b3577f2f33009addc2b30b7deb336a20ce37c
danny-blip <lmdn.villa@gmail.com>
2022-04-05 17:38:06 -0500

    memorama

--- memorama/script.js
+++ memorama/script.js
@@ -1,5 +1,6 @@
-let icons = []
-let selections = []
+let icons = [];
+let selections = [];
+let cardCount = 10;
 
 function loadIcons() {
     icons = [
@@ -57,28 +58,48 @@ function loadIcons() {
         '<img src = "./images/52.jpg">',
         '<img src = "./images/53.jpg">',
         '<img src = "./images/54.jpg">',
-    ]
+    ];
 }
 
-function generateBoard() {
-    var nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26];//all numbers to be randomized
-    ranNums = [];
-    a = nums.length;
-    b = 0;
-    while (a--) {
-        b = Math.floor(Math.random() * (a + 1));
-        ranNums.push(nums[b]);
-        nums.splice(b, 1);
+function generateBoard(level) {
+    if (level == 'l1') {
+        var nums = [2, 3, 5, 7, 11, 12, 13, 14, 16, 17, 19, 23, 24];//all numbers to be randomized
+        ranNums = [];
+        a = nums.length;
+        b = 0;
+        while (a--) {
+            b = Math.floor(Math.random() * (a + 1));
+            ranNums.push(nums[b]);
+            nums.splice(b, 1);
+        }
+    } else if (level == 'l2') {
+        var nums = [0, 1, 4, 6, 8, 9, 10, 15, 18, 20, 21, 22, 25, 26];//all numbers to be randomized
+        ranNums = [];
+        a = nums.length;
+        b = 0;
+        while (a--) {
+            b = Math.floor(Math.random() * (a + 1));
+            ranNums.push(nums[b]);
+            nums.splice(b, 1);
+        }
+    } else {
+        var nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26];//all numbers to be randomized
+        ranNums = [];
+        a = nums.length;
+        b = 0;
+        while (a--) {
+            b = Math.floor(Math.random() * (a + 1));
+            ranNums.push(nums[b]);
+            nums.splice(b, 1);
+        }
     }
-    //ranNums.next().value; //To use afterwards (for each time that the numbers are used)
-
-    loadIcons()
-    selections = []
-    let board = document.getElementById("board")
-    let cards = []
-    for (let i = 1; i < 25; i++) {
+    loadIcons();
+    selections = [];
+    let board = document.getElementById("board");
+    let cards = [];
+    console.log(ranNums);
+    for (let i = 1; i < (cardCount / 2) + 1; i++) {
         console.log(ranNums[i])
-        console.log(ranNums[i] + 27)
         cards.push(`
         <div class="card-area" onclick="selectCard(${i})">
             <div class="card" id="card${i}">
@@ -86,54 +107,73 @@ function generateBoard() {
                     ${icons[ranNums[i]]}
                 </div>
                 <div class="side front">
-                    <img src = "./images/REVERSO.png">
+                    <img src = "/images/REVERSO.png">
                 </div>
             </div>
         </div>        
         `)
         cards.push(`
-        <div class="card-area" onclick="selectCard(${i + 1})">
-            <div class="card" id="card${i + 1}">
-                <div class="side rear" id="rear${i + 1}">
+        <div class="card-area" onclick="selectCard(${i + 27})">
+            <div class="card" id="card${i + 27}">
+                <div class="side rear" id="rear${i + 27}">
                     ${icons[(ranNums[i]) + 27]}
                 </div>
                 <div class="side front">
-                    <img src = "./images/REVERSO.png">
+                    <img src = "/images/REVERSO.png">
                 </div>
             </div>
-        </div>        
-        `)
-        i++;
+        </div>
+        `);
     }
-    cards.sort(() => Math.random() - 0.5)
-    board.innerHTML = cards.join(" ")
+    cards.sort(() => Math.random() - 0.5);
+    board.innerHTML = cards.join(" ");
 }
 
 function selectCard(i) {
-    let card = document.getElementById("card" + i)
+    let card = document.getElementById("card" + i);
     if (card.style.transform != "rotateY(180deg)") {
-        card.style.transform = "rotateY(180deg)"
-        selections.push(i)
+        card.style.transform = "rotateY(180deg)";
+        selections.push(i);
     }
     if (selections.length == 2) {
-        deselect(selections)
-        selections = []
+        deselect(selections);
+        selections = [];
     }
 }
 
 function deselect(selections) {
     setTimeout(() => {
-        let rear1 = document.getElementById("rear" + selections[0])
-        let rear2 = document.getElementById("rear" + selections[1])
-        console.log(selections[0])
-        console.log(selections[1])
-        if(selections[0] + 1 == selections[1] || selections[0] == selections[1] + 1)
-            console.log('y')
+        let rear1 = document.getElementById("rear" + selections[0]);
+        let rear2 = document.getElementById("rear" + selections[1]);
+        console.log(selections[0]);
+        console.log(selections[1]);
+        if(selections[0] + 27 == selections[1] || selections[0] == selections[1] + 27) {
+            rear1.style.background = "plum";
+            rear2.style.background = "plum";
+            console.log('saca es putooooo')
+        }
         else {
-            let card1 = document.getElementById("card" + selections[0])
-            let card2 = document.getElementById("card" + selections[1])
-            card1.style.transform = "rotateY(0deg)"
-            card2.style.transform = "rotateY(0deg)"
+            let card1 = document.getElementById("card" + selections[0]);
+            let card2 = document.getElementById("card" + selections[1]);
+            card1.style.transform = "rotateY(0deg)";
+            card2.style.transform = "rotateY(0deg)";
+        }
+        if (verifyWin()) {
+            swal.fire({
+                title: `¡Has ganado!`,
+                text: `Has encontrado todas las cartas.`,
+                icon: `success`
+            });
         }
     }, 1000);
+}
+
+function verifyWin() {
+    for (let i = 1; i <= (cardCount / 2); i ++) {
+        let rear = document.getElementById("rear" + i);
+        if (rear.style.background != "plum") {
+            return false;
+        }
+    }
+    return true;
 }
