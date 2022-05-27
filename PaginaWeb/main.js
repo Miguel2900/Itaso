6a71863d90ee4867067798915731e2cfec0e604b
BaldingZeus6 <miguel.ledesmao29@gmail.com>
2022-05-27 14:35:30 -0500

    Fixes in result page

--- PaginaWeb/main.js
+++ PaginaWeb/main.js
@@ -1,7 +1,46 @@
 let azucarV = [16,/*Alimento lácteo bebible con probióticos naturales sabor fresa*/
   7,/*Agua de fruta con jugo natural de naranja*/
   1,/*Agua saborizada o agua infusionada*/
-];
+  48,/*Bebida de té verde sabor pepino con miel*/
+  21,/*Bebida de pulpa de mango*/
+  27,/*Bebida carbonatada de cola*/
+  24,/*Chocolate en polvo*/
+  6,/*Queso petit suisse sabor fresa*/
+  41,/*Yogurt bebible sabor fresa*/
+  47,/*Bebida energética*/
+  31,/*Bebida carbonatada sabor naranja*/
+  30,/*Bebida carboatada sabor toronja*/
+  42,/*Concentrado de horchata*/
+  28,/*Concentrado de jamaica*/
+  49,/*Néctar clarificado de manzana*/
+  31,/*Néctar de piña*/
+  45,/*Bebida carbonatada de cola*/
+  25,/*Leche sabor chocolate*/
+  33,/*Bebida a base de té negro sabor limón*/
+  29,/*Bebida carbonatada sabor manzana*/
+  0,/*Bebida carbonatada*/
+  37,/*Bebida deportiva*/
+  32,/*Bebida carbonatada sabor uva*/
+  0,/*Polvo para preparar agua de sabor*/
+  29,/*Bebida carbonatada sabor lima-limón*/
+  9,/*Producto a base de leche fermentada con lactobacilos*/
+  41,/*Yogurt bebible sabor fresa*/
+  0,/*Ron*/
+  17,/*Gin-tonic*/
+  0,/*Brandy/Coñac*/
+  0,/*Mezcal*/
+  4,/*Tinto de mesa*/
+  0,/*Whisky*/
+  15,/*Paloma*/
+  27,/*Mojito*/
+  4,/*Michelada clásica*/
+  19,/*Cuba*/
+  78,/*Piña colada*/
+  46,/*Margarita*/
+  2,/*Pulque natural*/
+  0,/*Martini seco*/
+  3,/*Regular*/];
+
 var edad, peso, altura;
 
 var maxBebidas
@@ -34,7 +73,7 @@ function generateBeverages() {
 
   let board = document.getElementById("board");
 
-  maxBebidas = (edad < 18) ? 28 : 46;
+  maxBebidas = (edad < 18) ? 28 : 43;
   for (let i = 1; i < maxBebidas; i++) {
     board.innerHTML += 
     `<div class="container">
@@ -53,9 +92,9 @@ function generateBeverages() {
 
 function sugarCalc() {
   let azucar = 0;
-  for (let i = 0; i < 27; i++) {
-    value = document.getElementById(`contar${(i + 1)}`).innerHTML;
-    azucar += value * azucarV[i];
+  for (let i = 1; i < maxBebidas; i++) {
+    value = document.getElementById(`contar${i}`).innerHTML;
+    azucar += value * azucarV[(i - 1)];
   }
 
   document.getElementById("display").innerHTML = "";
@@ -67,24 +106,42 @@ function sugarCalc() {
   display.appendChild(h1);
 
 
-  document.getElementById("display").innerHTML += `<br><img src="./front/Cuchara.png"><p>x${(azucar / 15).toFixed(1)} cucharadas soperas</p></img>`;
+  document.getElementById("display").innerHTML += `<br><img src="./front/Cuchara.png"><h1>x${(azucar / 15).toFixed(1)} cucharadas soperas</h1></img>`;
 
+  let color;
   if (azucar > 25) {
-    cDiario.id = "red";
-    cDiario.innerHTML += 'Tu consumo se excede del recomendado diario de 25 gramos';//cambio color
+    color = '#ff0000';
+    cDiario.innerHTML += `<div class="display2" id="textBox">
+      <h1/>Tu consumo se excede del recomendado <br>diario de 25 gramos
+      </div>`;
   }
   else {
-    cDiario.id = "green";
-    cDiario.innerHTML += 'Tu consumo se encuentra debajo del recomendado diario de 25 gramos';//cambio color
+    color = '#008000'
+    cDiario.innerHTML += `<div class="display2" id="textBox">
+      <h1/>Tu consumo se encuentra debajo del <br>recomendado diario de 25 gramos
+      </div>`;
   }
   display.appendChild(cDiario);
 
+  document.getElementById('textBox').style.backgroundColor = color;
+
+  if (azucar > 25) {
+    var blink = document.getElementById('textBox');
+    setInterval(function () {
+      blink.style.opacity = (blink.style.opacity == 0 ? 1 : 0);
+    }, 800);
+  }
+
+
+
   if (edad >= 18) {
     var IMC = document.createElement("h1");
-    IMC.innerHTML += "Su IMC es " + (peso / Math.pow(altura / 100, 2)).toFixed(3);
+    IMC.innerHTML += "<br><br>Su IMC es " + (peso / Math.pow(altura / 100, 2)).toFixed(3);
     display.appendChild(IMC);
   }
 }
+
+
 
 
 
