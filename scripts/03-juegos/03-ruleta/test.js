848c463b13ec4284b0046086e90cb0908ac7d8c3
BaldingZeus6 <miguel.ledesmao29@gmail.com>
2022-10-05 23:09:43 -0500

    Ruleta fix

--- scripts/03-juegos/03-ruleta/test.js
+++ scripts/03-juegos/03-ruleta/test.js
@@ -1,3 +1,14 @@
+var pieces = document.getElementsByClassName("draggable");
+var div=document.getElementById("foodContainer");
+
+
+for (var i = 0; i < pieces.length; i++) {
+  pieces[i].style.position="absolute";
+  pieces[i].style.left = (Math.random() * (div.offsetWidth - 30 - 1)) + 1 + 'px';
+  pieces[i].style.top = (Math.random() * (div.offsetHeight - 1)) + 1 + 'px';
+  console.log(pieces[i].style.top);
+}
+
 function circumPointFromAngle(cx, cy, r, a) {
   return [
     cx + r * Math.cos(a),
@@ -5,10 +16,9 @@ function circumPointFromAngle(cx, cy, r, a) {
   ];
 }
 
-
 // configuration values
-var radius = 40;
-var center = 60;
+var radius = 50;
+var center = 50;
 
 // out actual data
 var data = [
@@ -43,55 +53,55 @@ var data = [
 var svg = document.querySelector('#svg');
 
 // reduce the values to get the max and then use it to map the values to ratios
-var values = data.map(function(record){ return record.value; })
-var max = values.reduce(function(pre, cur) { return pre + cur; });
-var ratios = values.map(function(val) { return val / max; });
+var values = data.map(function (record) { return record.value; })
+var max = values.reduce(function (pre, cur) { return pre + cur; });
+var ratios = values.map(function (val) { return val / max; });
 
 // starting angle (this is -90° because svg counts zero as +90° to common sense)
 var theta = -Math.PI * 0.5;
 
 // for each ratio, lets build our segment
-ratios.forEach(function(val, idx) {
-  
+ratios.forEach(function (val, idx) {
+
   // Get the angle of the segment (delta)
   var delta = val * 2 * Math.PI;
-  
+
   // also get the normal from the center of this segment (for transition)
   var normal = theta + delta * 0.5;
-  
+
   // These are the points on the circumfrence that the arc starts and ends
   var startPos = circumPointFromAngle(center, center, radius, theta);
   var endPos = circumPointFromAngle(center, center, radius, theta + delta);
-  
+
   // Add the current delta to theta for the next segment
   theta += delta;
-  
+
   // if the arc is greater than 180° we need to use the large arc flag
   var largeArc = '0 0 1';
   if (delta >= Math.PI) {
     largeArc = '0 1 0';
   }
-  
+
   // for convenience store each path command in an array
   var actions = [];
   actions.push('M ' + center + ' ' + center);
   actions.push('L ' + startPos.join(' '));
   actions.push('A ' + radius + ' ' + radius + ' ' + largeArc + ' ' + endPos.join(' '));
   actions.push('Z');
-  
+
   // create the element, set the d attr, data attrs and fill style
   var path = document.createElementNS("http://www.w3.org/2000/svg", "path");
   path.setAttribute('d', actions.join('\n'));
   path.setAttribute('label', data[idx].label);
   path.setAttribute('ondragover', 'allowDrop(event)');
   path.setAttribute('ondrop', 'drop(event)');
-  path.setAttribute('id', 'path-'+idx);
+  path.setAttribute('id', 'path-' + idx);
   path.style.fill = data[idx].color;
 
-  var text = document.createElementNS("http://www.w3.org/2000/svg","text");
-  var textPath = document.createElementNS("http://www.w3.org/2000/svg","textPath");
-  textPath.textContent+="Testal;sdmgpoasdjfpoajsdopgjasdopgj";
-  textPath.setAttribute('href','textcircle');
+  var text = document.createElementNS("http://www.w3.org/2000/svg", "text");
+  var textPath = document.createElementNS("http://www.w3.org/2000/svg", "textPath");
+  textPath.textContent += "Testal;sdmgpoasdjfpoajsdopgjasdopgj";
+  textPath.setAttribute('href', 'textcircle');
 
   text.appendChild(textPath)
   svg.appendChild(text);
@@ -99,6 +109,9 @@ ratios.forEach(function(val, idx) {
 
 });
 
+function game(){
+  
+}
 
 
 function allowDrop(ev) {
@@ -108,7 +121,6 @@ function allowDrop(ev) {
 function drag(ev) {
   ev.dataTransfer.setData("foodGroup", ev.target.getAttribute("foodGroup"));
   ev.dataTransfer.setData("name", ev.target.getAttribute("id"));
-  console.log( ev.target.getAttribute("id"));
 }
 
 function drop(ev) {
@@ -118,15 +130,51 @@ function drop(ev) {
   validateFood(ev.target.getAttribute("label"), foodGroup, foodName);
 }
 
-function validateFood(group, foodGroup, foodName)
-{
+function validateFood(group, foodGroup, foodName) {
   if (foodGroup == group) {
-    window.alert(foodName + " es un " + foodGroup + " y conincide con un " + group);
-    food=document.getElementById(foodName);
-    food.style.visibility="hidden";
+    food = document.getElementById(foodName);
+    food.style.visibility = "hidden";
   }
-  else
-  {
-    window.alert(foodName + " no es un " + group + " es un " + foodGroup);
-  }
+}
+
+const rulette = document.querySelector("#ruleta");
+
+rulette.addEventListener("click", girar);
+spins = 0;
+
+function girar() {
+  let rand = Math.random() * 7200;
+  calcular(rand);
+}
+
+function calcular(rand) {
+  valor = rand / 360;
+  valor = (valor - parseInt(valor.toString().split(".")[0])) * 360;
+  console.log(valor);
+  ruleta.style.transform = "rotate(" + rand + "deg)";
+
+  setTimeout(() => {
+    switch (true) {
+      case (valor > 0 && valor <= 36) || (valor <= 360 && valor > 324) :
+        console.log("Verduras");
+        //premio("Verduras");
+        break;
+      case valor > 36 && valor <= 108:
+        console.log("Cereales");
+        //premio("Frutas");
+        break;
+      case valor > 108 && valor <= 180:
+        console.log("Leguminosas");
+        //premio("Cereales");
+        break;
+      case valor > 180 && valor <= 252:
+        console.log("Animal");
+        //premio("Leguminosas");
+        break;
+      case valor > 252 && valor <= 324:
+        console.log("Frutas");
+        //premio("Origen animal");
+        break;
+    }
+  }, 5000);
 }
